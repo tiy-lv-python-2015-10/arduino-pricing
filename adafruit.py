99271675e0948b41cb8a309979705954dab2f1b9
jp <jp.park@me.com>
2015-11-25 01:03:40 -0800

    2nd try

--- adafruit.py
+++ adafruit.py
@@ -1,1 +1,27 @@
-__author__ = 'JP'
+import requests as r
+from bs4 import BeautifulSoup
+
+response = r.get("https://www.adafruit.com/category/17")
+response.content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Product ID
+soup.find_all('div', 'product_id')
+
+# Price
+soup.find_all('span', 'normal-price')
+
+# Name
+# all names show and duplicate names
+for link in soup.find_all('a'):
+    print(link.get('data-name'))
+
+# Link to detail page
+for product in soup.find_all('a'):
+    print(product.get('href'))
+
+# How manay arein stock(0 if Out of Stock)
+soup.find_all('div', class_='stock')
+
+# URL of the product image
+soup.find_all(name='img')
